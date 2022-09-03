80e78c7548a7f36dfb0d27f123e464aa6b5458a5
SpringSrikanth <srikanthyenagagandula89@gmail.com>
2022-09-04 00:11:24 +0530

    updated app.ts

--- app.js
+++ app.js
@@ -4,40 +4,43 @@ const multer = require("multer");
 const path = require("path");
 const cors = require('cors');
 
-// storage engine 
-
 const storage = multer.diskStorage({
     destination: './upload/images',
     filename: (req, file, cb) => {
-        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+        const extname = path.extname(file.originalname);
+        return cb(null, `${file.originalname.replace(extname,'')}_${Date.now()}${path.extname(file.originalname)}`)
     }
 })
 
 const upload = multer({
     storage: storage,
     limits: {
-        fileSize: 1000558888
+        fileSize: 1024 * 1024 * 100 /** Accepts maximum 100 MB Data */
     }
 })
 app.use(cors()); //enabling for all origins
-app.use('/profile', express.static('upload/images'));
-app.post("/upload", upload.single('profile'), (req, res) => {
-
+app.use('/uploaded-file-details', express.static('upload/images'));
+app.post("/upload", upload.single('file'), (req, res) => {
+    const host = req.headers.host || "http://localhost:4000"
     res.json({
-        success: 1,
-        profile_url: `http://localhost:4000/profile/${req.file.filename}`
+        success: 200,
+        message: "File uploaded successfully",
+        filename: req.file.filename,
+        download_url: `${host}/uploaded-file-details/${req.file.filename}`
     })
 })
 
 function errHandler(err, req, res, next) {
     if (err instanceof multer.MulterError) {
         res.json({
-            success: 0,
-            message: err.message
+            success: 500,
+            message: err.message,
+            filename: req.file.filename,
         })
     }
 }
 app.use(errHandler);
+
 app.listen(4000, () => {
     console.log("server up and running");
 })
